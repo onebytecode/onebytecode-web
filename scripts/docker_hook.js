f8901f2b088f65fe7cfdb42bb0ce92d4d7848d19
CashALot <nikitagoncharovmtu@yandex.ru>
2017-05-14 15:11:13 +0300

    docker hook changed link

--- scripts/docker_hook.js
+++ scripts/docker_hook.js
@@ -1,5 +1,5 @@
 var request = require('request')
-var _uri = 'https://registry.hub.docker.com/u/devpalsorg/musicconnects-web/trigger/6729851e-7695-44d4-9260-da8ddb10c956/'
+var _uri = 'https://registry.hub.docker.com/u/onebytecode/mainweb/trigger/26624aaa-6186-4dbc-b136-9cea1f93a1e2/'
 console.log('Hoocking');
 request({
   method: 'POST',
