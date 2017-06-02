ee211ea23f62631eb5950e6a78053ef11736c095
Alexander <alex.danilin.ad@gmail.com>
2017-06-02 17:13:58 +0300

    pre release

--- components/header.jsx
+++ components/header.jsx
@@ -4,7 +4,7 @@ export default class Header extends React.Component {
   render() {
     return (
       <header id="header" className="alt">
-        <h1>OneByteCode DEEEEEEV </h1>
+        <h1>OneByteCode</h1>
         <p>Нас всего двое<br/>И мы ищем недостающие биты!</p>
       </header>
     )
