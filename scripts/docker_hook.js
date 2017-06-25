10229c2a194efe447c05c611b785b8bf08a2359c
goncharovnikita <goncharovnikita.dev@gmail.com>
2017-06-25 16:57:05 +0300

    docker hook fix

--- scripts/docker_hook.js
+++ scripts/docker_hook.js
@@ -1,13 +1,19 @@
 var request = require('request')
 var _uri = 'https://registry.hub.docker.com/u/onebytecode/mainweb/trigger/26624aaa-6186-4dbc-b136-9cea1f93a1e2/'
+
 console.log('Hoocking');
 request({
-  method: 'POST',
-  uri: _uri,
-  multipart: [
-      {
-        'content-type': 'application/json',
-        body: JSON.stringify({})
-      }
-    ],
-})
+    method: 'POST',
+    uri: _uri,
+    json: {
+      "source_type":"Branch",
+      "source_name":"develop"
+    }
+
+  },
+  function (error, response, body) {
+    if (error) {
+      return console.error('upload failed:', error);
+    }
+    console.log('Hook successful!  Server responded with:', body);
+  })
